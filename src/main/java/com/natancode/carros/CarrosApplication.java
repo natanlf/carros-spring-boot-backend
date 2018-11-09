f2d992d84430f269a17035c0dd0b2d182a1d5be9
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-11-09 10:25:08 -0200

    Projeto criado

---
+++ src/main/java/com/natancode/carros/CarrosApplication.java
@@ -0,0 +1,12 @@
+package com.natancode.carros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrosApplication.class, args);
+	}
+}
