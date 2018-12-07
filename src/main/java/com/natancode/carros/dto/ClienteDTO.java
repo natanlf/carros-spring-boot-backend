e2f6ab9b17a4371a0b819ea2bb0fd4d42221170d
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-07 19:40:24 -0200

    Validacao update cliente

--- src/main/java/com/natancode/carros/dto/ClienteDTO.java
+++ src/main/java/com/natancode/carros/dto/ClienteDTO.java
@@ -2,6 +2,11 @@ package com.natancode.carros.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.natancode.carros.domain.Cliente;
 
 public class ClienteDTO implements Serializable {
@@ -9,7 +14,13 @@ public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="Email inválido")
 	private String email;
 
 	public ClienteDTO() {
