b44d568181cfe9ba489b0f89db1648eb4577577a
Natan Lara Ferreira <natanlaraferreira@gmail.com>
2018-12-12 20:33:51 -0200

    Criando profile de desenvolvimento. Corrigir horario verão

---
+++ src/main/java/com/natancode/carros/config/DevConfig.java
@@ -0,0 +1,34 @@
+package com.natancode.carros.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.natancode.carros.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService; 
+	
+	//pego o valor da propeties par saber se crio o banco novamente, se atualizo, faço nada...
+		@Value("${spring.jpa.hibernate.ddl-auto}")
+		private String strategy; 
+		
+		@Bean
+		public boolean instantiateDatabase() throws ParseException {
+			
+			//se não for para criar o bd retorta false e não faz nada
+			if(!"create".equals(strategy))
+				return false;
+			
+			dbService.instantiateTestDatabase(); //instancio a classe que popula o banco
+			return true;
+		}
+}
